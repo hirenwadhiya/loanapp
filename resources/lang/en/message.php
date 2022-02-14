ccb2f96c2038e3491982802531cc6256ef21970a
Hiren Wadhiya <hirenwadhiya@gmail.com>
2022-02-14 09:42:32 +0530

    common response messages

---
+++ resources/lang/en/message.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'user' => [
+        'create' => [
+            'success' => 'User created successfully',
+            'error' => 'Error in creating user'
+        ]
+    ]
+];
